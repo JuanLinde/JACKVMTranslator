70925fc557837d41b3e549466be2290fc7610297
Juan Linde <jslinde123@gmail.com>
2021-11-02 16:06:43 -0400

    Implemented and tested the writeArithmetic() method from the CodeWriter class along with all of its sub-functions.

--- testVMTranslator.cpp
+++ testVMTranslator.cpp
@@ -10,10 +10,12 @@ class Parser
 {
 private:
 	ifstream vmCode;
+	string currentInstruction;
 	string currentCommand;
 	string currentCommandType;
 	string currentModifier;
 	int currentIndex;
+
 	/*
 		Functionality: Determines if the current line being traversed contains an instruction. If
 					   it does, returns true.
@@ -74,6 +76,7 @@ public:
 	Parser(string fileName);
 	~Parser() { vmCode.close(); }
 
+	string getCurrentInstruction() { return currentInstruction; }
 	string getCurrentCommand() { return currentCommand; }
 	string getCurrentCommandType() { return currentCommandType; }
 	string getCurrentModifier() { return currentModifier; }
@@ -92,26 +95,70 @@ public:
 	*/
 	void advance();
 };
+/*
+	This class contains all the methods necessary to translate an instruction from JACK VM code
+	to HACK assembly language and output the translation into an output file.
+*/
+class CodeWriter
+{
+private:
+	ofstream assemblyCode;
+
+	// Helper functions
+	/*
+		Functionality: Receives an arithmetic command and outputs the corresponding translation to
+		               the output file containing the code in HACK assembly language.
+	*/
+	void writeArithmeticPop(string);
+public:
+	CodeWriter(string fileName) { assemblyCode.open(fileName); }
+	~CodeWriter() { assemblyCode.close(); }
+
+	/*
+		Functionality: Receives a string, c, that contains an arithmetic command in JACK VM
+					   language, and outputs the translation to HACK assembly to the output file.
+	*/
+	void writeArithmetic(string);
+	/*
+		Functionality: Receives a string, c, that contains a command in JACK VM language; a string,
+		s, that contains the virtual memory segment; and a an int, ind, that refers to the index
+		of the virtual memory segment to be accessed. And outputs the corresponding HACK assembly
+		code to the output file
+	*/
+	void writePushPop(string, string, int);
+	/*
+		Functionality: Receives a string with the current command, c, and writes the corresponding
+		               HACK assembly operation for that command.
+	*/
+	void writeArithmeticOp(string);
+	/*
+		Functionality: Writes the commands in the HACK assembly language to increment the stack
+		               pointer by one. This is done after any arithmetic operation to keep the 
+					   pointer in the right place.
+	*/
+	void writeUpdateStack();
+};
 
 int main(int argc, char* argv[])
 {
 	string inputFileName = argv[1];
+	string outputFileName = argv[2];
 	Parser parser(inputFileName);
-	ofstream outputCode("outputTest.txt");
+	CodeWriter writer(outputFileName);
 
 	while (parser.hasMoreLines())
 	{
 		parser.advance();
 		string commandType = parser.getCurrentCommandType();
 
-		bool commandIsReturn = (commandType == "C_RETURN");
-		if (!commandIsReturn)
+		bool commandTypeIsNotReturn = (commandType != "C_RETURN");
+		bool thereIsCommand = (commandType != "");
+		if (thereIsCommand && commandTypeIsNotReturn)
 		{
-			string command = parser.getCurrentCommand();
-
 			bool commandIsArithmetic = (commandType == "C_ARITHMETIC");
 			bool commandIsPushPop = (commandType == "C_PUSH" || commandType == "C_POP");
 
+			string command = parser.getCurrentCommand();
 			if (commandIsArithmetic) writer.writeArithmetic(command);
 			//else if (commandIsPushPop)
 			//{
@@ -126,6 +173,8 @@ int main(int argc, char* argv[])
 	}
 	return 0;
 }
+
+// Parser class methods
 /*
 	Functionality: Receives the command type of the current instruction, cT, and returns true
 				   or false depending upon the syntax of the command type.
@@ -141,6 +190,7 @@ bool Parser::currentCommandHasModifier()
 Parser::Parser(string fileName)
 {
 	vmCode.open(fileName);
+	currentInstruction = "";
 	currentCommand = "";
 	currentCommandType = "";
 	currentModifier = "";
@@ -162,23 +212,25 @@ void Parser::advance()
 
 	if (HasInstruction(currentLine))
 	{
-		currentCommand = extractCommandFrom(currentLine);
+		currentInstruction = currentLine;
+		currentCommand = extractCommandFrom(currentInstruction);
 		currentCommandType = extractCommandTypeFrom(currentCommand);
 
 		if (currentCommandHasModifier())
 		{
-			currentModifier = extractModifier(currentLine);
+			currentModifier = extractModifier(currentInstruction);
 		}
 		else currentModifier = "";
 
 		if (currentInstructionHasIndex())
 		{
-			currentIndex = extractIndex(currentLine); 
+			currentIndex = extractIndex(currentInstruction); 
 		}
 		else currentIndex = -1;
 	}
 	else
 	{
+		currentInstruction = "";
 		currentCommand = "";
 		currentCommandType = "";
 		currentModifier = "";
@@ -338,3 +390,95 @@ int Parser::extractIndex(string cL)
 	int index = stoi(cL.substr(secondSpacePos + 1));
 	return index;
 }
+
+// CodeWriter class methods
+
+/*
+	Functionality: Receives a string, c, that contains an arithmetic command in JACK VM
+				   language, and outputs the translation to HACK assembly to the output file.
+
+*/
+void CodeWriter::writeArithmetic(string c)
+{
+	writeArithmeticPop(c);        // Writes the popping of arguments from the stack
+	writeArithmeticOp(c);         // Writes the appropiate operations
+}
+
+/*
+		Functionality: Receives an arithmetic command and outputs the corresponding translation to
+					   the output file containing the code in HACK assembly language.
+*/
+void CodeWriter::writeArithmeticPop(string c)
+{
+	bool commandIsUnary(c == "neg" || c == "not");
+	if (commandIsUnary)
+	{
+		assemblyCode << "// Pops the first element from the stack" << endl;
+		assemblyCode << "@sp" << endl;
+		assemblyCode << "M=M-1" << endl;
+		assemblyCode << "A=M" << endl;
+	}
+	else
+	{
+		assemblyCode << "// Pops the first element from the stack" << endl;
+		assemblyCode << "@sp" << endl;
+		assemblyCode << "M=M-1" << endl;
+		assemblyCode << "A=M" << endl;
+		assemblyCode << "D=M" << endl; 
+
+		assemblyCode << "// Pops the second element from the stack" << endl; 
+		assemblyCode << "@sp" << endl;
+		assemblyCode << "M=M-1" << endl;
+		assemblyCode << "A=M" << endl;
+	}
+}
+/*
+	Functionality: Receives a string with the current command, c, writes the corresponding
+				   HACK assembly operation for that command and the corresponding update to the
+				   stack.
+*/
+void CodeWriter::writeArithmeticOp(string c)
+{
+	bool commandIsComparison = (c == "eq" || c == "lt" || c == "gt");
+	if (commandIsComparison)
+	{
+		/*
+			Functionality: Modifies commands for output purposes.
+		*/
+		if (c == "eq") c = "EQ";
+		else if (c == "lt") c = "LT";
+		else c = "GT";
+
+		assemblyCode << "// Sets up the comparison for jumping" << endl;
+		assemblyCode << "D=M-D" << endl;
+		assemblyCode << "// Compares and executes accordingly" << endl;
+		assemblyCode << "@" << c << endl;
+		assemblyCode << "D;J" << c << endl;
+		assemblyCode << "M=0" << endl;
+		writeUpdateStack();                    // writes the update to the pointer of the stack
+		assemblyCode << "(" << c << ")" << endl;
+		assemblyCode << "M=1" << endl;
+		writeUpdateStack();
+	}
+	else
+	{
+		if (c == "add") assemblyCode << "M=D+M" << endl;
+		else if (c == "sub") assemblyCode << "M=D-M" << endl;
+		else if (c == "neg") assemblyCode << "M=-M" << endl;
+		else if (c == "and") assemblyCode << "M=D&M" << endl;
+		else if (c == "or")  assemblyCode << "M=D|M" << endl;
+		else assemblyCode << "M=!M" << endl;
+		writeUpdateStack(); 
+	}
+}
+/*
+		Functionality: Writes the commands in the HACK assembly language to increment the stack
+					   pointer by one. This is done after any arithmetic operation to keep the
+					   pointer in the right place.
+*/
+void CodeWriter::writeUpdateStack()
+{
+	assemblyCode << "@sp" << endl;
+	assemblyCode << "M=M+1" << endl;
+}
+
